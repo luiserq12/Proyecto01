2a82f7a513f4e79d01d09893ee9b787f40f9dc3a
luiserq12 <luis.ostos@softtek.com>
2017-03-29 16:50:47 -0600

    Delete Helloworld.java

--- Helloworld.java
+++
@@ -1,9 +0,0 @@
-package com.git.examen;
-public class Helloworld {
-
-    public static void main(String[] args) {
-        System.out.println("Hello GitHub!");
-     // Luis Enrique Ostos Ríos - Examen GIT
-    }
-
-}
